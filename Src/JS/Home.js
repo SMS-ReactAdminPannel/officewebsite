738551d0e6ea82f6229af227bf05d009d9b74110
Vigneshwari3108 <vigneshwari.yoho@gmail.com>
2025-06-20 12:46:23 +0530

    changes

--- Src/JS/Home.js
+++ Src/JS/Home.js
@@ -28,31 +28,3 @@ $(document).ready(function() {
   });
 
 })
-
-
-let currentIndex = 0;
-  const track = document.getElementById("carousel-track");
-  const items = track.children;
-  const visibleCount = 5;
-
-  function updateActive() {
-    for (let i = 0; i < items.length; i++) {
-      items[i].classList.remove("active");
-    }
-    const center = currentIndex + Math.floor(visibleCount / 2);
-    if (items[center]) items[center].classList.add("active");
-  }
-
-  function moveSlide(direction) {
-    const totalItems = items.length;
-    if (currentIndex + direction >= 0 && currentIndex + visibleCount + direction <= totalItems) {
-      currentIndex += direction;
-      const offset = (100 / visibleCount) * currentIndex;
-      track.style.transform = `translateX(-${offset}%)`;
-      updateActive();
-
-      
-    }
-  }
-
-    updateActive(); // set initial
